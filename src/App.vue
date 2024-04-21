a6e89fee3e65db6caa1b906672f3304404deeddf
wagenrace <nijhof.dns@gmail.com>
2024-04-21 21:53:33 +0200

    Remove hello world

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,5 @@ import ButtonCartonRight from './components/button/buttonCartonRight.vue';
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
   <ButtonCartonRight />
 </template>
